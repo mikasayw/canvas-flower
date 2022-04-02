69a1fca18726b41576d61c7a9b850448771bba43
mikasayw <mikasayw@163.com>
2022-04-02 16:02:52 +0800

    feat: init point

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <canvas ref="element" class="canvas" />
+  <canvas ref="element" class="canvas" :style="`width:${WIDTH/2};height:${HEIGHT/2}px;`" />
 </template>
 
 <script lang="ts" setup>
@@ -18,6 +18,13 @@ interface Point {
   y: number
 }
 
+const startPointArray: Array<Point> = [
+  { x: 0, y: HEIGHT * Math.random() },
+  { x: WIDTH, y: HEIGHT * Math.random() },
+  { x: WIDTH * Math.random(), y: 0 },
+  { x: WIDTH * Math.random(), y: HEIGHT },
+]
+
 onMounted(() => {
   element.width = WIDTH
   element.height = HEIGHT
@@ -25,18 +32,60 @@ onMounted(() => {
 })
 
 function init() {
-  ctx.strokeStyle = 'rgba(107,114,128,.3)'
+  startPointArray.forEach((item) => {
+    step(getStartPoint(item))
+  })
+}
 
-  const startPoint: Branch = {
-    start: { x: WIDTH / 2, y: HEIGHT },
-    length: 2,
-    angle: -Math.PI / 2,
+function getStartPoint({ x, y }: Point): Branch {
+  let _angel = Math.PI
+
+  if (x === 0) {
+    switch (y) {
+      case 0:
+        _angel = _angel / (3 + Math.random() * 3)
+        break
+      case HEIGHT:
+        _angel = -_angel / (3 + Math.random() * 3)
+        break
+      default:
+        _angel = 0
+        break
+    }
+  }
+  else if (x === WIDTH) {
+    switch (y) {
+      case 0:
+        _angel = _angel / (1 + Math.random())
+        break
+      case HEIGHT:
+        _angel = -_angel / (1 + Math.random())
+        break
+      default:
+        break
+    }
+  }
+  else {
+    switch (y) {
+      case 0:
+        _angel /= 2
+        break
+      case HEIGHT:
+        _angel /= -2
+        break
+      default:
+        break
+    }
   }
 
-  step(startPoint)
+  return {
+    start: { x, y },
+    length: Math.random() * 6,
+    angle: _angel,
+  }
 }
-const pendingTask: Array<Function> = []
 
+const pendingTask: Array<Function> = []
 function step(b: Branch, depth = 0) {
   drawBranch(b)
   const end = getEndPoint(b)
@@ -45,7 +94,7 @@ function step(b: Branch, depth = 0) {
     pendingTask.push(() => {
       step({
         start: end,
-        length: 2 + Math.random() * 10 - 5,
+        length: Math.random() * 10 + Math.random() * 10 - 5,
         angle: b.angle + Math.random() * 0.3,
       }, depth + 1)
     })
@@ -54,7 +103,7 @@ function step(b: Branch, depth = 0) {
     pendingTask.push(() => {
       step({
         start: end,
-        length: 2 + Math.random() * 10 - 5,
+        length: Math.random() * 10 + Math.random() * 10 - 5,
         angle: b.angle - Math.random() * 0.3,
       }, depth + 1)
     })
@@ -79,7 +128,7 @@ function loopFrame() {
 }
 loopFrame()
 
-function getEndPoint(b: Branch) {
+function getEndPoint(b: Branch): Point {
   const { start, length, angle } = b
   return {
     x: start.x + length * Math.cos(angle),
@@ -92,6 +141,7 @@ function drawBranch(b: Branch) {
 }
 
 function lineTo(p1: Point, p2: Point) {
+  ctx.strokeStyle = 'rgba(107,114,128,.3)'
   ctx.beginPath()
   ctx.moveTo(p1.x + 0.1, p1.y + 0.1)
   ctx.lineTo(p2.x + 0.1, p2.y + 0.1)
@@ -113,7 +163,5 @@ body {
   position: fixed;
   top: 0;
   left: 0;
-  width: 100%;
-  height: 100%;
 }
 </style>
